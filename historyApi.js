f2298dac456f9898dd02a88a1197caad57b4a90d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-03 23:11:31 +0600

    History api to navigate back, forward, go, history length

---
+++ historyApi.js
@@ -0,0 +1,15 @@
+/*  
+    --history.back() : going back to history in browser
+    --history.forward() : going forward to history in browser
+
+We can go back and forward and reload with history.go() method:
+    --history.go(-1) : one step going back
+    --history.go(1) : one step going forward
+    --history.go() : reload the page
+
+    Use case of history:
+        -- sign in page
+        -- sign in with information
+        -- after sign in then back to previous page
+
+*/
