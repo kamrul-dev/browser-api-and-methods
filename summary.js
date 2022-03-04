b3ccfac8618265c98bd8c85df8308f3a3530ca99
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-04 22:13:18 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,40 @@
+/* 
+ -- document works with browser
+ -- alert()
+ -- confirm()
+ -- prompt()
+ -- url explain, 
+ -- location.reload()
+ -- location.href = ''
+ -- location.hash =
+ -- history.back()
+ -- history.go()
+ -- cookies
+ -- sessionStorage vs localStorage
+ -- cookies vs sessionStorage vs localStorage
+ -- browser web api
+ -- 
+*/
+
+
+/* 
+    contenEditable
+    ==============
+    -- document.body.contentEditable
+    -- document.body.contentEditable = true
+
+    open a new Tab
+    ==============
+    -- chrome://newtab
+
+    task running on chrome
+    ======================
+    -- shift + esc
+
+    chrome restart
+    ===============
+    -- chrome://restart
+
+
+
+*/
